27d26c98d299fe180ac8f4c828561d017e1e0e7a
ali m alghanay <a.al-ghanay@uot.edu.ly>
2023-05-20 00:54:26 +0200

    completed, ready for upload to domin #1

---
+++ scr/clock.js
@@ -0,0 +1,14 @@
+var conutDownDate = new Date("Jul 01, 2023 00:00:00").getTime();
+var x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = conutDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000)
